33e867402a850327a13199ea924b0d9d051e5536
Shubham Jawandhiya <shubham@criodo.com>
2020-03-15 12:21:41 +0530

    Initial commit

---
+++ precache-manifest.e039df5849b880c125d8ff9807fbba51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08e0a59f4eb966c07e44070d057e61e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cd57c3d6ac113bd3171",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "29f8dbcb37259b661232",
+    "url": "/static/css/main.16a6ff49.chunk.css"
+  },
+  {
+    "revision": "7cd57c3d6ac113bd3171",
+    "url": "/static/js/2.2e4adad3.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/static/js/2.2e4adad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f8dbcb37259b661232",
+    "url": "/static/js/main.a78c6cc0.chunk.js"
+  },
+  {
+    "revision": "7ffe976c74292b1255f7",
+    "url": "/static/js/runtime-main.8261f036.js"
+  }
+]);
